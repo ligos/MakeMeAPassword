5619d3fc7412e41ec6cbf0f9cf6909f0f782c04d
Murray Grant <mujgrant@ligos.net>
2014-01-19 22:08:27 +1100

    Check client side we're on https as well.

--- MakeMeAPassword.Web/Content/scripts/site-main.js
+++ MakeMeAPassword.Web/Content/scripts/site-main.js
@@ -1,4 +1,17 @@
-// Copyright Murray Grant 2013
+// Copyright 2014 Murray Grant
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 // A global argument to the password get event (below);
 window.pwg = window.pwg || {};
@@ -248,6 +261,12 @@ $(document).on('change', '#passwordParameters input', function (evt) {
 });
 
 $(document).ready(function () {
+    // Make darned sure we're on the https version.
+    if (window.location.hostname.toLowerCase() !== 'localhost' && window.location.protocol.toLowerCase() !== 'https:') {
+        window.location.href = window.location.href.replace(window.location.protocol, 'https:');
+        return;
+    }
+
     // Automatically trigger getting combinations and a default password on each of the specific pages.
     var getBtn = $('#passwordGet');
     if (getBtn.length) {
