f239a69b7abc50e2764c5b115a6912e9c6196dfb
Murray Grant <none@none>
2016-04-05 11:57:46 +1000

    Add benchmark project to test random generator performance.

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<RandomServiceBenchmarks>();
+        }
+    }
+}
---
+++ Benchmarks/RandomServiceBenchmarks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Columns;
+
+using MurrayGrant.PasswordGenerator.Web.Services;
+
+namespace Benchmarks
+{
+    [Config(typeof(RandomServiceBenchmarks.Config))]
+    public class RandomServiceBenchmarks
+    {
+        private static readonly byte[] _Seed = new byte[32];
+        private readonly RandomService _Random = new RandomService(_Seed);
+
+        private class Config : ManualConfig 
+        { 
+            public Config()
+            { 
+                Add(Job.LegacyJitX86, Job.RyuJitX64);
+                Add(StatisticColumn.AllStatistics);
+            } 
+        }
+
+        [Benchmark()]
+        public int Random_Int32()
+        {
+            return _Random.Next();
+        }
+        [Benchmark()]
+        public float Random_Single()
+        {
+            return _Random.NextSingle();
+        }
+        [Benchmark()]
+        public byte[] Random_1024Bytes()
+        {
+            return _Random.GetNextBytes(1024);
+        }
+    }
+}
