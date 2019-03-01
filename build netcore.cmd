9d9e10403002eaf79c5135e3e5df0ff1696da62a
Murray Grant <bitbucket@ligos.net>
2019-03-01 21:04:22 +1100

    Change build script to work better with SCP sync.
    --HG--
    branch : dotnetcore

--- build netcore.cmd
+++ build netcore.cmd
@@ -4,9 +4,9 @@ dotnet clean -c Release
 rem Publish for .NET Core 2.1
 dotnet publish Web.NetCore -c Release -f netcoreapp2.1 
 
-rem Build deployment 7z file for .NET Core 2.1, without config files
-del /q MakeMeAPassword.netcoreapp21.7z
-cd Web.NetCore\bin\Release\netcoreapp2.1\publish
-"C:\Program Files\7-Zip\7z.exe" a -mx1 -x!appsettings*.json -x!nlog.config -x!web.config ..\..\..\..\..\MakeMeAPassword.netcoreapp21.7z * 
+rem Delete config files so we can deploy
+del /q Web.NetCore\bin\Release\netcoreapp2.1\publish\appsettings*.json
+del /q Web.NetCore\bin\Release\netcoreapp2.1\publish\nlog.config
+del /q Web.NetCore\bin\Release\netcoreapp2.1\publish\web.config
 
 pause
