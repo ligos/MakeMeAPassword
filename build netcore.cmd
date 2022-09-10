6ab219b0f189a57c233037b8a6a89bdf544f5b2d
Murray Grant <github@ligos.net>
2022-09-10 21:14:39 +1000

    Ready for deployment!

--- build netcore.cmd
+++ build netcore.cmd
@@ -1,13 +1,12 @@
 rem Clean before building
 dotnet clean -c Release
 
-rem Publish for .NET Core 3.1
-dotnet publish Web.NetCore -c Release -f netcoreapp3.1 
+rem Publish for .NET 6.0
+dotnet publish Web.Net60 -c Release -f net6.0
 
 rem Delete config files so we can deploy
-del /q Web.NetCore\bin\Release\netcoreapp3.1\publish\appsettings*.json
-del /q Web.NetCore\bin\Release\netcoreapp3.1\publish\nlog.config
-del /q Web.NetCore\bin\Release\netcoreapp3.1\publish\web.config
-del /q Web.NetCore\bin\Release\netcoreapp3.1\publish\bypasskeys.txt
+del /q Web.Net60\bin\Release\net6.0\publish\appsettings*.json
+del /q Web.Net60\bin\Release\net6.0\publish\nlog.config
+del /q Web.Net60\bin\Release\net6.0\publish\web.config
 
 pause
