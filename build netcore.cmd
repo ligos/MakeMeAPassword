b5584b56a3d70172ec8fcaea9b8ebfdbf158ba7b
Murray Grant <github@ligos.net>
2024-04-21 20:55:25 +1000

    Change to build net80 version

--- build netcore.cmd
+++ build netcore.cmd
@@ -1,12 +1,12 @@
 rem Clean before building
 dotnet clean -c Release
 
-rem Publish for .NET 6.0
-dotnet publish Web.Net60 -c Release -f net6.0
+rem Publish for .NET 8.0
+dotnet publish Web -c Release -f net8.0
 
 rem Delete config files so we can deploy
-del /q Web.Net60\bin\Release\net6.0\publish\appsettings*.json
-del /q Web.Net60\bin\Release\net6.0\publish\nlog.config
-del /q Web.Net60\bin\Release\net6.0\publish\web.config
+del /q Web\bin\Release\net8.0\publish\appsettings*.json
+del /q Web\bin\Release\net8.0\publish\nlog.config
+del /q Web\bin\Release\net8.0\publish\web.config
 
 pause
