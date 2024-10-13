4f2b6a3f1cb75634eaa2df0839d7b628d3f9381e
Murray Grant <github@ligos.net>
2024-10-13 17:47:59 +1100

    Bump versions for actual project

--- build netcore.cmd
+++ build netcore.cmd
@@ -2,11 +2,11 @@ rem Clean before building
 dotnet clean -c Release
 
 rem Publish for .NET 8.0
-dotnet publish Web -c Release -f net8.0
+dotnet publish Web -c Release -f net80
 
 rem Delete config files so we can deploy
-del /q Web\bin\Release\net8.0\publish\appsettings*.json
-del /q Web\bin\Release\net8.0\publish\nlog.config
-del /q Web\bin\Release\net8.0\publish\web.config
+del /q Web\bin\Release\net80\publish\appsettings*.json
+del /q Web\bin\Release\net80\publish\nlog.config
+del /q Web\bin\Release\net80\publish\web.config
 
 pause
